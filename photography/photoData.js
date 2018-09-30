f893af97c7f34b4085d7b7877aac38abedb2fce0
Alex Warnes <alex.s.warnes@gmail.com>
2018-09-30 09:55:04 -0400

    data file for gallery photos; loads with photography index html

---
+++ photography/photoData.js
@@ -0,0 +1,6 @@
+const galleryJSON = [
+    {"thumbURL": "https://instagram.com/p/BegSGKllXz4/media/?size=t", "fullResURL": "https://instagram.com/p/BegSGKllXz4/media/?size=l", "alt": "A trail in Olympic National Park"},
+    {"thumbURL": "https://instagram.com/p/BdnSNjIFS2i/media/?size=t", "fullResURL": "https://instagram.com/p/BdnSNjIFS2i/media/?size=l", "alt": "Seville"},
+    {"thumbURL": "https://instagram.com/p/BVZ1f6_FsFl/media/?size=t", "fullResURL": "https://instagram.com/p/BVZ1f6_FsFl/media/?size=l", "alt": "Telescope Cafe in Paris"},
+    {"thumbURL": "https://instagram.com/p/BYEdb7tF44P/media/?size=t", "fullResURL": "https://instagram.com/p/BYEdb7tF44P/media/?size=l", "alt": "Milky Way Core"}
+]
