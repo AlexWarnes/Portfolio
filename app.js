cf0be75f914b6c9eb337490d507ddd57f02c0cb9
Alex Warnes <alex.s.warnes@gmail.com>
2018-09-30 09:53:32 -0400

    added photogrid load, gallery controls; changed function declarations

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const STATE = {
     currentView: undefined
 };
 
-const STARTUP = () => {
+function STARTUP() {
     loadNavBar();
     loadMenu();
     setCurrentView();
@@ -12,9 +12,17 @@ const STARTUP = () => {
     burgerOnClick();
     menuLinkOnClick();
     loadError();
+
+    if (STATE.currentView === 'photography') {
+        loadGrid(galleryJSON);
+        thumbnailOnClick();
+        nextPhotoOnClick();
+        prevPhotoOnClick();
+        closePhotoOnClick();
+    }
 };
 
-const loadNavBar = () => {
+function loadNavBar() {
     $('#navBar-js').html(`
         <nav class="nav-bar background-white" role="navigation">
             <div class="burger-box background-dkblue white">
@@ -52,11 +60,11 @@ const loadNavBar = () => {
             </div>
         </nav>
     `);
-    $('#navBar-js').fadeIn(600);
+    $('#navBar-js').fadeIn(400);
 };
 
 
-const loadMenu = () => {
+function loadMenu() {
     $('#menu-js').html(`
     <section class="menu background-dkblue white" role="navigation" aria-hidden="true" aria-live="assertive">
         <a href="/" class="white menu-link">
@@ -115,7 +123,7 @@ const loadMenu = () => {
     `);
 };
 
-const setCurrentView = () => {
+function setCurrentView() {
     let path = window.location.pathname;
 	let view;
     path.includes('webdevelopment')
@@ -131,7 +139,7 @@ const setCurrentView = () => {
     STATE.currentView = view;
 };
 
-const highlightCurrentView = () => {
+function highlightCurrentView() {
     switch(STATE.currentView) {
         case 'webdev':
             return $('.nl-webdev>.nav-tab').addClass('current-tab-webdev');
@@ -146,31 +154,31 @@ const highlightCurrentView = () => {
     };
 };
 
-const toggleAriaHidden = () => {
+function toggleAriaHidden() {
     $('.menu').attr('aria-hidden') == 'false' 
         ? $('.menu').attr('aria-hidden', true) 
         : $('.menu').attr('aria-hidden', false);
 };
 
-const toggleMenu = () => {
+function toggleMenu() {
     $('.burger, .menu').toggleClass('open');
     toggleAriaHidden();
 }
 
-const burgerOnClick = () => {
+function burgerOnClick() {
     $('.burger-box').on('click', (e) => {
         e.preventDefault();
         toggleMenu();
     });
 };
 
-const menuLinkOnClick = () => {
+function menuLinkOnClick() {
     $('.menu-link').on('click', (e) => {
         toggleMenu();
     })
 }
 
-const loadError = () => {
+function loadError() {
     $('#error-js').html(`
     <div class="error">
         <i class="material-icons display-icon error-icon">error_outline</i>
@@ -203,5 +211,128 @@ const loadError = () => {
     `);
     $('#error-js').slideToggle();
 };
+
+function loadGrid(data) {
+    data.map((photo, index) => {
+        let thumb = renderThumbnail(photo, index);
+        $('.photo-grid').append(thumb);
+        $(`#photo-${index}`).on('load', function(){
+            $(`#photo-${index}`).fadeIn(600);
+        })    
+    });
+}
+
+function renderThumbnail(photo, index) {
+    return `
+        <div class="photo-tile">
+            <img src="${photo.thumbURL}" alt="${photo.alt}" id="photo-${index}" class="photo-thumb">
+        </div>`;
+}
+
+function changeToLargeUrl(instaUrl) {
+    return instaUrl.slice(0, -1) + 'l';
+}
+
+function openGallery() {
+    $('.pictureFrame-js').fadeIn().css('display', 'flex');
+}
+
+function closeGallery() {
+    $('.pictureFrame-js').fadeOut();
+    $('.photo').attr('src', '');
+    $('.photo').attr('alt', '');
+}
+
+function displayFullRes(data){
+    $('.photo').attr('src', data.fullResURL);
+    $('.photo').attr('alt', data.alt);
+    $('.photo').on('load', () => {
+        $('.photo').fadeIn(200);
+    });
+    //Anytime a full res photo is showing, we listen for arrows
+    switchPhotoOnArrow();
+}
+
+function thumbnailOnClick() {
+    $('.photo-thumb').on('click', (e) => {
+        let selectedPhoto = getThisPhotoData($(e.currentTarget).attr('src'));
+        openGallery();
+        displayFullRes(selectedPhoto);
+    })
+}
+
+function getThisPhotoData(currentPhoto) {
+    let currentIndex = galleryJSON.findIndex(item => item.thumbURL === currentPhoto);
+    return galleryJSON[currentIndex];
+}
+
+function getNextPhotoData(currentPhoto) {
+    let currentIndex = galleryJSON.findIndex(item => item.fullResURL === currentPhoto);
+    let nextIndex
+    if (currentIndex === galleryJSON.length - 1) {
+        nextIndex = 0;
+    } else {
+        nextIndex = currentIndex + 1;
+    } 
+    console.log(`CURRENT INDEX: ${currentIndex}`);
+    console.log(`NEXT INDEX: ${nextIndex}`);
+    return galleryJSON[nextIndex];
+}
+
+function getPrevPhotoData(currentPhoto) {
+    let currentIndex = galleryJSON.findIndex(item => item.fullResURL === currentPhoto);
+    let prevIndex
+    if (currentIndex === 0) {
+        prevIndex = galleryJSON.length - 1;
+    } else {
+        prevIndex = currentIndex - 1;
+    } 
+    console.log(`CURRENT INDEX: ${currentIndex}`);
+    console.log(`PREV INDEX: ${prevIndex}`);
+    return galleryJSON[prevIndex];
+}
+
+function nextPhotoOnClick() {
+    $('.next-photo-js').on('click', (e) => {
+        console.log("NEXT PLZ")
+        let nextPhoto = getNextPhotoData($('.photo').attr('src'));
+        displayFullRes(nextPhoto);
+    })
+}
+
+function prevPhotoOnClick() {
+    $('.prev-photo-js').on('click', (e) => {
+        console.log("PREV PLZ")
+        let prevPhoto = getPrevPhotoData($('.photo').attr('src'));
+        displayFullRes(prevPhoto);
+    })
+}
+
+function closePhotoOnClick() { 
+    $('.close-frame-js').on('click', (e) => {
+        closeGallery();
+    });
+}
+
+function switchPhotoOnArrow(){
+    console.log('LISTENING FOR ARROWS')
+    $('body').one('keydown', (e) => {
+        let keyName = e.key;
+        console.log(keyName);
+        switch(keyName){
+            case 'ArrowRight':
+                let nextPhoto = getNextPhotoData($('.photo').attr('src'));
+                return displayFullRes(nextPhoto);
+            case 'ArrowLeft':
+                let prevPhoto = getPrevPhotoData($('.photo').attr('src'));
+                return displayFullRes(prevPhoto);
+            case 'Escape':
+                return closeGallery();
+            default:
+                //In case any other key is pressed, we continue listening for arrows
+                return switchPhotoOnArrow();
+        }
+    });    
+} 
 
 $(STARTUP);
