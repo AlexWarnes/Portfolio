2c0bf5681ccb3d332aacaad47e0dc0edc6295fcb
Alex Warnes <alex.s.warnes@gmail.com>
2018-09-30 17:33:35 -0400

    started JSON from which to load gallery dynamically

--- photography/photoData.js
+++ photography/photoData.js
@@ -1,6 +1,31 @@
 const galleryJSON = [
-    {"thumbURL": "https://instagram.com/p/BegSGKllXz4/media/?size=t", "fullResURL": "https://instagram.com/p/BegSGKllXz4/media/?size=l", "alt": "A trail in Olympic National Park"},
+    {"thumbURL": "https://instagram.com/p/BYEdb7tF44P/media/?size=t", "fullResURL": "https://instagram.com/p/BYEdb7tF44P/media/?size=l", "alt": "Milky Way Core"},
+    {"thumbURL": "https://instagram.com/p/BamvwWTFBpp/media/?size=t", "fullResURL": "https://instagram.com/p/BamvwWTFBpp/media/?size=l", "alt": "Clouds Parting around the Milky Way core"},
+    {"thumbURL": "https://instagram.com/p/BUGBcI2Fzxv/media/?size=t", "fullResURL": "https://instagram.com/p/BUGBcI2Fzxv/media/?size=l", "alt": "Standing under a starry sky with a lantern"},
+    {"thumbURL": "https://instagram.com/p/BSwXZHMF1IV/media/?size=t", "fullResURL": "https://instagram.com/p/BSwXZHMF1IV/media/?size=l", "alt": "Fish eye view of the Milky Way core in New Mexico"},
+    {"thumbURL": "https://instagram.com/p/BUTNpKZlFlH/media/?size=t", "fullResURL": "https://instagram.com/p/BUTNpKZlFlH/media/?size=l", "alt": "Stars over Lake Tahoe"},
+
+    {"thumbURL": "https://instagram.com/p/BeLhkzIFJMJ/media/?size=t", "fullResURL": "https://instagram.com/p/BeLhkzIFJMJ/media/?size=l", "alt": "Colorful and intricate patterns in Alhambra"},
+    {"thumbURL": "https://instagram.com/p/Bd_GFBdlCf4/media/?size=t", "fullResURL": "https://instagram.com/p/Bd_GFBdlCf4/media/?size=l", "alt": "Reflection, symmetry, and lines inside the Nazrid Palaces of Alhambra"},
+    {"thumbURL": "https://instagram.com/p/BjPDZhZhH3L/media/?size=t", "fullResURL": "https://instagram.com/p/BjPDZhZhH3L/media/?size=l", "alt": "Doors in Alhambra"},
     {"thumbURL": "https://instagram.com/p/BdnSNjIFS2i/media/?size=t", "fullResURL": "https://instagram.com/p/BdnSNjIFS2i/media/?size=l", "alt": "Seville"},
-    {"thumbURL": "https://instagram.com/p/BVZ1f6_FsFl/media/?size=t", "fullResURL": "https://instagram.com/p/BVZ1f6_FsFl/media/?size=l", "alt": "Telescope Cafe in Paris"},
-    {"thumbURL": "https://instagram.com/p/BYEdb7tF44P/media/?size=t", "fullResURL": "https://instagram.com/p/BYEdb7tF44P/media/?size=l", "alt": "Milky Way Core"}
+    {"thumbURL": "https://instagram.com/p/Bd5sQAbl622/media/?size=t", "fullResURL": "https://instagram.com/p/Bd5sQAbl622/media/?size=l", "alt": "Beautiful arches and architecture in Plaza de España, Seville, Spain"},
+
+    {"thumbURL": "https://instagram.com/p/BWY9sAMFD6x/media/?size=t", "fullResURL": "https://instagram.com/p/BWY9sAMFD6x/media/?size=l", "alt": "View of Yosemite Valley"},
+    {"thumbURL": "https://instagram.com/p/BWWbwyFlL99/media/?size=t", "fullResURL": "https://instagram.com/p/BWWbwyFlL99/media/?size=l", "alt": "A castle and church overlooking Lake Bled in Slovenia"},
+    {"thumbURL": "https://instagram.com/p/BegSGKllXz4/media/?size=t", "fullResURL": "https://instagram.com/p/BegSGKllXz4/media/?size=l", "alt": "A trail in Olympic National Park"},
+    {"thumbURL": "https://instagram.com/p/BifRyqYhNEk/media/?size=t", "fullResURL": "https://instagram.com/p/BifRyqYhNEk/media/?size=l", "alt": "White clouds floating above the Rose City of Petra, Jordan"},
+    {"thumbURL": "https://instagram.com/p/BeVdwgMltvv/media/?size=t", "fullResURL": "https://instagram.com/p/BeVdwgMltvv/media/?size=l", "alt": "Clouds and trees somewhere along the winding mountain highways of California"},
+    
+    {"thumbURL": "https://instagram.com/p/BU-R_ZvFEo9/media/?size=t", "fullResURL": "https://instagram.com/p/BU-R_ZvFEo9/media/?size=l", "alt": "San Francisco skyline at night"},
+    {"thumbURL": "https://instagram.com/p/BVdz6SFFkZa/media/?size=t", "fullResURL": "https://instagram.com/p/BVdz6SFFkZa/media/?size=l", "alt": "Arc de Triomphe at night"},
+    {"thumbURL": "https://instagram.com/p/BVbazwKlR6W/media/?size=t", "fullResURL": "https://instagram.com/p/BVbazwKlR6W/media/?size=l", "alt": "Eiffel Tower with the lights off, under the stars"},
+    {"thumbURL": "https://instagram.com/p/BLpjP4hAT4G/media/?size=t", "fullResURL": "https://instagram.com/p/BLpjP4hAT4G/media/?size=l", "alt": "Chain Bridge at night in Budapest, Hungary"},
+    {"thumbURL": "https://instagram.com/p/BHY0hYYAE1l/media/?size=t", "fullResURL": "https://instagram.com/p/BHY0hYYAE1l/media/?size=l", "alt": "International Space Station passing over Northern Virginia"},
+    
+    {"thumbURL": "https://instagram.com/p/BVZ1f6_FsFl/media/?size=t", "fullResURL": "https://instagram.com/p/BVZ1f6_FsFl/media/?size=l", "alt": "Telescope Cafe in Paris"}
 ]
+
+//TODO: Add specific gallery "tags" (astro, landscape, paris, etc)
+//then from the Photography page, a selected gallery will filter
+//over the appropriate imgs to include
