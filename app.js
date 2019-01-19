a7588be69b35cb8c2bfc360b14eb7b7ec654a5cc
Alex Warnes <alex.s.warnes@gmail.com>
2019-01-18 23:55:07 -0500

    began converting to plain js

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 'use strict';
 
 const STATE = {
-    currentView: undefined
+  currentView: undefined
 };
 
-function STARTUP() {
-    loadNavBar();
-    loadMenu();
-    setCurrentView();
-    highlightCurrentView();
-    burgerOnClick();
-    menuLinkOnClick();
-    loadError();
+document.addEventListener('DOMContentLoaded', () => {
+  loadNavBar();
+  loadMenu();
+  setCurrentView();
+  highlightCurrentView();
+  burgerOnClick();
+  menuLinkOnClick();
+  loadError();
 
-    if (STATE.currentView === 'photography') {
-        loadGrid(galleryJSON);
-        thumbnailOnClick();
-        nextPhotoOnClick();
-        prevPhotoOnClick();
-        closePhotoOnClick();
-        keyboardControls();
-    }
-};
+  if (STATE.currentView === 'photography') {
+    loadGrid(galleryJSON);
+    thumbnailOnClick();
+    nextPhotoOnClick();
+    prevPhotoOnClick();
+    closePhotoOnClick();
+    keyboardControls();
+  }
+});
+
+const $ = (element) => document.querySelector(element);
 
 function loadNavBar() {
-    $('#navBar-js').html(`
+    $('#navBar-js').innerHTML = `
         <nav class="nav-bar background-white" role="navigation">
             <div class="burger-box background-dkblue white">
                 <div class="burger">
@@ -60,13 +62,13 @@ function loadNavBar() {
                 </a>
             </div>
         </nav>
-    `);
-    $('#navBar-js').fadeIn(400);
+    `;
+    // $('#navBar-js').fadeIn(400);
 };
 
 
 function loadMenu() {
-    $('#menu-js').html(`
+    $('#menu-js').innerHTML = `
     <section class="menu background-dkblue white" role="navigation" aria-hidden="true" aria-live="assertive">
         <a href="/" class="white menu-link">
             <i class="material-icons">home</i>
@@ -122,7 +124,7 @@ function loadMenu() {
     
         </div>
     </section>
-    `);
+    `;
 };
 
 function setCurrentView() {
@@ -144,200 +146,198 @@ function setCurrentView() {
 function highlightCurrentView() {
     switch(STATE.currentView) {
         case 'webdev':
-            return $('.nl-webdev>.nav-tab').addClass('current-tab-webdev');
+            return $('.nl-webdev>.nav-tab').classList.add('current-tab-webdev');
         case 'photography':
-            return $('.nl-photography>.nav-tab').addClass('current-tab-photography')
+            return $('.nl-photography>.nav-tab').classList.add('current-tab-photography')
         case 'resources':
-            return $('.nl-resources>.nav-tab').addClass('current-tab-resources')
+            return $('.nl-resources>.nav-tab').classList.add('current-tab-resources')
         case 'about':
-            return $('.nl-about>.nav-tab').addClass('current-tab-about')
+            return $('.nl-about>.nav-tab').classList.add('current-tab-about')
         default:
             return null;
     };
 };
 
 function toggleAriaHidden() {
-    $('.menu').attr('aria-hidden') == 'false' 
-        ? $('.menu').attr('aria-hidden', true) 
-        : $('.menu').attr('aria-hidden', false);
+  if ($('.menu').attributes['aria-hidden'].value === 'false') {
+    $('.menu').setAttribute('aria-hidden', 'true') 
+  } else {
+    $('.menu').setAttribute('aria-hidden', 'false');
+  }
 };
 
 function toggleMenu() {
-    $('.burger, .menu').toggleClass('open');
-    toggleAriaHidden();
+  $('.burger').classList.toggle('open');
+  $('.menu').classList.toggle('open');
+  toggleAriaHidden();
 }
 
 function burgerOnClick() {
-    $('.burger-box').on('click', (e) => {
-        e.preventDefault();
-        toggleMenu();
-    });
+  $('.burger-box').addEventListener('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
 };
 
 function menuLinkOnClick() {
-    $('.menu-link').on('click', (e) => {
-        toggleMenu();
-    })
+  $('.menu-link').addEventListener('click', (e) => {
+    toggleMenu();
+  })
 }
 
 function loadError() {
-    $('#error-js').html(`
+  $('#error-js').innerHTML = `
     <div class="error">
-        <i class="material-icons display-icon error-icon">error_outline</i>
-        <p>Sorry for the hiccup! I'm updating major branches of the site and haven't made it here yet.</p>
-        <p>Feel free to connect with me on any other platform, as I'm always interested in new projects or feedback. Thanks for visiting!</p>
-        <div role="region" class="introTile social-media">
+      <i class="material-icons display-icon error-icon">error_outline</i>
+      <p>Sorry for the hiccup! I'm updating major branches of the site and haven't made it here yet.</p>
+      <p>Feel free to connect with me on any other platform, as I'm always interested in new projects or feedback. Thanks for visiting!</p>
+      <div role="region" class="introTile social-media">
+        <a href="mailto:alex.s.warnes@gmail.com?subject=Contact%20Email" class="white" aria-label="Email" rel="noopener noreferrer">
+          <i class="far fa-envelope social-icon"></i>
+        </a>
 
-                <a href="mailto:alex.s.warnes@gmail.com?subject=Contact%20Email" class="white" aria-label="Email" rel="noopener noreferrer">
-                    <i class="far fa-envelope social-icon"></i>
-                </a>
-    
-                <a href="https://github.com/AlexWarnes" target="_blank" class="white" aria-label="GitHub" rel="noopener noreferrer">
-                    <i class="fab fa-github social-icon"></i>
-                </a>
-    
-                <a href="https://www.instagram.com/alexwarnesphotos/" target="_blank" class="white" aria-label="Instagram" rel="noopener noreferrer">
-                    <i class="fab fa-instagram social-icon"></i>
-                </a>
-    
-                <a href="https://www.linkedin.com/in/alexwarnes/" target="_blank" class="white" aria-label="LinkedIn" rel="noopener noreferrer">
-                    <i class="fab fa-linkedin-in social-icon"></i>
-                </a>
-    
-                <a href="https://twitter.com/a_warnes" target="_blank" class="white" aria-label="Twitter" rel="noopener noreferrer">
-                    <i class="fab fa-twitter social-icon"></i>
-                </a>
-    
-            </div>
+        <a href="https://github.com/AlexWarnes" target="_blank" class="white" aria-label="GitHub" rel="noopener noreferrer">
+          <i class="fab fa-github social-icon"></i>
+        </a>
+
+        <a href="https://www.instagram.com/alexwarnesphotos/" target="_blank" class="white" aria-label="Instagram" rel="noopener noreferrer">
+          <i class="fab fa-instagram social-icon"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/alexwarnes/" target="_blank" class="white" aria-label="LinkedIn" rel="noopener noreferrer">
+          <i class="fab fa-linkedin-in social-icon"></i>
+        </a>
+
+        <a href="https://twitter.com/a_warnes" target="_blank" class="white" aria-label="Twitter" rel="noopener noreferrer">
+          <i class="fab fa-twitter social-icon"></i>
+        </a>
+      </div>
     </div>
-    `);
-    $('#error-js').slideToggle();
+  `;
 };
 
 //========== PHOTOGRAPHY PAGE & GALLERY CONTROLS ==========
 // ========================================================
 
-function renderThumbnail(photo, index) {
-    return `
-        <div class="photo-tile">
-            <img src="${photo.thumbURL}" alt="${photo.alt}" id="photo-${index}" class="photo-thumb">
-        </div>`;
-}
+// function renderThumbnail(photo, index) {
+//     return `
+//         <div class="photo-tile">
+//             <img src="${photo.thumbURL}" alt="${photo.alt}" id="photo-${index}" class="photo-thumb">
+//         </div>`;
+// }
 
-function openGallery() {
-    $('.gallery-js').fadeIn().css('display', 'flex');
-}
+// function openGallery() {
+//     $('.gallery-js').fadeIn().css('display', 'flex');
+// }
 
-function closeGallery() {
-    $('.gallery-js').fadeOut();
-    $('.photo').attr('src', '');
-    $('.photo').attr('alt', '');
-}
+// function closeGallery() {
+//     $('.gallery-js').fadeOut();
+//     $('.photo').attr('src', '');
+//     $('.photo').attr('alt', '');
+// }
 
-function displayFullRes(data){
-    $('.photo').attr('src', data.fullResURL);
-    $('.photo').attr('alt', data.alt);
-    $('.photo').on('load', () => {
-        $('.photo').fadeIn(400);
-    });
-}
+// function displayFullRes(data){
+//     $('.photo').attr('src', data.fullResURL);
+//     $('.photo').attr('alt', data.alt);
+//     $('.photo').on('load', () => {
+//         $('.photo').fadeIn(400);
+//     });
+// }
 
-function getThisPhotoData(currentPhoto) {
-    let currentIndex = galleryJSON.findIndex(item => item.thumbURL === currentPhoto);
-    return galleryJSON[currentIndex];
-}
+// function getThisPhotoData(currentPhoto) {
+//     let currentIndex = galleryJSON.findIndex(item => item.thumbURL === currentPhoto);
+//     return galleryJSON[currentIndex];
+// }
 
-function getNextPhotoData(currentPhoto) {
-    let currentIndex = galleryJSON.findIndex(item => item.fullResURL === currentPhoto);
-    let nextIndex
-    if (currentIndex === galleryJSON.length - 1) {
-        nextIndex = 0;
-    } else {
-        nextIndex = currentIndex + 1;
-    } 
-    console.log(`%cCURRENT INDEX: ${currentIndex}`, "color: green; font-weight: bold;");
-    console.log(`%cNEXT INDEX: ${nextIndex}`, "font-weight: bold;");
-    return galleryJSON[nextIndex];
-}
+// function getNextPhotoData(currentPhoto) {
+//     let currentIndex = galleryJSON.findIndex(item => item.fullResURL === currentPhoto);
+//     let nextIndex
+//     if (currentIndex === galleryJSON.length - 1) {
+//         nextIndex = 0;
+//     } else {
+//         nextIndex = currentIndex + 1;
+//     } 
+//     console.log(`%cCURRENT INDEX: ${currentIndex}`, "color: green; font-weight: bold;");
+//     console.log(`%cNEXT INDEX: ${nextIndex}`, "font-weight: bold;");
+//     return galleryJSON[nextIndex];
+// }
 
-function getPrevPhotoData(currentPhoto) {
-    let currentIndex = galleryJSON.findIndex(item => item.fullResURL === currentPhoto);
-    let prevIndex
-    if (currentIndex === 0) {
-        prevIndex = galleryJSON.length - 1;
-    } else {
-        prevIndex = currentIndex - 1;
-    } 
-    console.log(`%cCURRENT INDEX: ${currentIndex}`, "color: green; font-weight: bold;");
-    console.log(`%cPREV INDEX: ${prevIndex}`, "font-weight: bold;");
-    return galleryJSON[prevIndex];
-}
+// function getPrevPhotoData(currentPhoto) {
+//     let currentIndex = galleryJSON.findIndex(item => item.fullResURL === currentPhoto);
+//     let prevIndex
+//     if (currentIndex === 0) {
+//         prevIndex = galleryJSON.length - 1;
+//     } else {
+//         prevIndex = currentIndex - 1;
+//     } 
+//     console.log(`%cCURRENT INDEX: ${currentIndex}`, "color: green; font-weight: bold;");
+//     console.log(`%cPREV INDEX: ${prevIndex}`, "font-weight: bold;");
+//     return galleryJSON[prevIndex];
+// }
 
-function showNextPhoto() {
-    let nextPhoto = getNextPhotoData($('.photo').attr('src'));
-    displayFullRes(nextPhoto);
-}
+// function showNextPhoto() {
+//     let nextPhoto = getNextPhotoData($('.photo').attr('src'));
+//     displayFullRes(nextPhoto);
+// }
 
-function showPrevPhoto() {
-    let prevPhoto = getPrevPhotoData($('.photo').attr('src'));
-    displayFullRes(prevPhoto);
-}
+// function showPrevPhoto() {
+//     let prevPhoto = getPrevPhotoData($('.photo').attr('src'));
+//     displayFullRes(prevPhoto);
+// }
 
-function loadGrid(data) {
-    data.map((photo, index) => {
-        let thumb = renderThumbnail(photo, index);
-        $('.photo-grid').append(thumb);
-        $(`#photo-${index}`).on('load', () => {
-            $(`#photo-${index}`).fadeIn(600);
-        });
-    });
-}
+// function loadGrid(data) {
+//     data.map((photo, index) => {
+//         let thumb = renderThumbnail(photo, index);
+//         $('.photo-grid').append(thumb);
+//         $(`#photo-${index}`).on('load', () => {
+//             $(`#photo-${index}`).fadeIn(600);
+//         });
+//     });
+// }
 
-function thumbnailOnClick() {
-    $('.photo-thumb').on('click', (e) => {
-        let selectedPhoto = getThisPhotoData($(e.currentTarget).attr('src'));
-        openGallery();
-        displayFullRes(selectedPhoto);
-    })
-}
+// function thumbnailOnClick() {
+//     $('.photo-thumb').on('click', (e) => {
+//         let selectedPhoto = getThisPhotoData($(e.currentTarget).attr('src'));
+//         openGallery();
+//         displayFullRes(selectedPhoto);
+//     })
+// }
 
-function nextPhotoOnClick() {
-    $('.next-photo-js').on('click', (e) => {
-        showNextPhoto();
-    });
-}
+// function nextPhotoOnClick() {
+//     $('.next-photo-js').on('click', (e) => {
+//         showNextPhoto();
+//     });
+// }
 
-function prevPhotoOnClick() {
-    $('.prev-photo-js').on('click', (e) => {
-        showPrevPhoto();
-    });
-}
+// function prevPhotoOnClick() {
+//     $('.prev-photo-js').on('click', (e) => {
+//         showPrevPhoto();
+//     });
+// }
 
-function closePhotoOnClick() { 
-    $('.close-frame-js').on('click', (e) => {
-        closeGallery();
-    });
-}
+// function closePhotoOnClick() { 
+//     $('.close-frame-js').on('click', (e) => {
+//         closeGallery();
+//     });
+// }
 
-function keyboardControls(){
-    $('body').one('keyup', (e) => {
-        let keyName = e.key;
-        //in each case, reset the listener after a key is pressed
-        switch(keyName){
-            case 'ArrowRight':
-                keyboardControls();
-                return showNextPhoto();
-            case 'ArrowLeft':
-                keyboardControls();
-                return showPrevPhoto();
-            case 'Escape':
-                keyboardControls();
-                return closeGallery();
-            default:
-                //In case any other key is pressed, reset the listener
-                return keyboardControls();
-        }
-    });    
-} 
-
-$(STARTUP);
+// function keyboardControls(){
+//     $('body').one('keyup', (e) => {
+//         let keyName = e.key;
+//         //in each case, reset the listener after a key is pressed
+//         switch(keyName){
+//             case 'ArrowRight':
+//                 keyboardControls();
+//                 return showNextPhoto();
+//             case 'ArrowLeft':
+//                 keyboardControls();
+//                 return showPrevPhoto();
+//             case 'Escape':
+//                 keyboardControls();
+//                 return closeGallery();
+//             default:
+//                 //In case any other key is pressed, reset the listener
+//                 return keyboardControls();
+//         }
+//     });    
+// 
